cf680ff80cc84c6181f306f7f49e8b0a168e4a7b
darinjbrown <darin.brown.cs@gmail.com>
2024-04-02 20:29:25 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import DateCounter from './DateCounter';
+import Header from './Header';
 
 export default function App() {
 	return (
-		<div>
-			<DateCounter />
+		<div className='app'>
+			<Header />
+
+			<main className='main'></main>
 		</div>
 	);
 }
